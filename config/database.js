f7641336693c08f3aa02d2bebf383cce86af92a2
Jaafar Mortada <mortada9190@gmail.com>
2025-03-11 14:51:15 +0200

    update database name

--- config/database.js
+++ config/database.js
@@ -1,29 +1,29 @@
 const mysql = require("mysql2/promise");
 
 var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "roottoor",
-    database: "angular-pos",
-    multipleStatements: true,
-    dateStrings: true,
+	connectionLimit: 10,
+	host: "127.0.0.1",
+	user: "morexa",
+	password: "5~q^ukf0iIU4",
+	database: "morexa",
+	multipleStatements: true,
+	dateStrings: true,
 });
 
 pool.getConnection(async (err, connection) => {
-    if (err) {
-        if (err.code === "PROTOCOL_CONNECTION_LOST") {
-            console.error("Database connection was closed.");
-        }
-        if (err.code === "ER_CON_COUNT_ERROR") {
-            console.error("Database has too many connections.");
-        }
-        if (err.code === "ECONNREFUSED") {
-            console.error("Database connection was refused.");
-        }
-    }
-    if (connection) await connection.release();
-    return;
+	if (err) {
+		if (err.code === "PROTOCOL_CONNECTION_LOST") {
+			console.error("Database connection was closed.");
+		}
+		if (err.code === "ER_CON_COUNT_ERROR") {
+			console.error("Database has too many connections.");
+		}
+		if (err.code === "ECONNREFUSED") {
+			console.error("Database connection was refused.");
+		}
+	}
+	if (connection) await connection.release();
+	return;
 });
 
 module.exports = pool;
